75e8fdd8572f680c084ce6b9f8132e3eb73f5ff8
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-08-24 17:40:51 -0600

    Merge and sort months

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -3,6 +3,27 @@ use std::{collections::HashMap, fs::OpenOptions};
 use anyhow::{anyhow, Result};
 use proyecto_bd::*;
 
+const SPACING: usize = 12;
+
+fn mes_a_int(mes: &str) -> usize {
+    match mes {
+        "NA" => 0,
+        "Enero" => 1,
+        "Febrero" => 2,
+        "Marzo" => 3,
+        "Abril" => 4,
+        "Mayo" => 5,
+        "Junio" => 6,
+        "Julio" => 7,
+        "Agosto" => 8,
+        "Septiembre" => 9,
+        "Octubre" => 10,
+        "Noviembre" => 11,
+        "Diciembre" => 12,
+        _ => 13,
+    }
+}
+
 fn main() -> Result<()> {
     #[cfg(feature = "dhat")]
     let _profiler = dhat::Profiler::new_heap();
@@ -18,12 +39,15 @@ fn main() -> Result<()> {
         let (file, out) = reader_writer(&file, "./results/out_temp.csv")?;
         remove_empty_lines(file, out)?;
     } else {
-        println!("Warning: File './results/out_temp.csv' already exists, skipping '{file}' cleanup")
+        println!(
+            "{:>SPACING$} File './results/out_temp.csv' already exists, skipping '{file}' cleanup",
+            "Warning"
+        )
     }
 
     // Obtenemos los valores únicos
     let (file, mut out) = reader_writer("./results/out_temp.csv", "./results/uniques.json")?;
-    let (headers, uniques) = {
+    let (headers, mut uniques) = {
         let mut lines = file.lines();
 
         let headers = lines.next().unwrap()?.to_owned();
@@ -36,21 +60,53 @@ fn main() -> Result<()> {
         (headers, u)
     };
 
+    // Hacer que ambos conjuntos de meses sean iguales, para despues tener un solo `mes.csv`
+    uniques.iter_mut().for_each(|(a, b)| {
+        if a.contains("mes") {
+            b.insert("NA".to_string());
+        }
+    });
+
+    // Es necesario iterar siempre en el mismo orden, asi que conjelamos en conjunto en un vector
     let mut uniques: HashMap<_, Vec<_>> = uniques
         .into_iter()
         .map(|(k, v)| (k, v.into_iter().collect()))
         .collect();
 
-    uniques.values_mut().for_each(|v| v.sort());
+    // Ordenamos todos los vectores de valores alfabéticamente
+    uniques.iter_mut().for_each(|(k, v)| {
+        if k.contains("mes") {
+            // Ordenamos manualmente los meses, esta chido tener 1 es enero, 12 diciembre
+            v.sort_by(|a, b| mes_a_int(a).cmp(&mes_a_int(b)))
+        } else {
+            v.sort()
+        }
+    });
 
     write!(out, "{:#?}", uniques)?;
-    println!("Wrote unique values to './results/uniques.json'");
+    println!(
+        "{:>SPACING$} Wrote unique values to './results/uniques.json'",
+        "Status"
+    );
 
     // Reemplazamos todos los valores repetidos por el id
     let (file, mut out) = reader_writer("./results/out_temp.csv", "./results/out.csv")?;
 
     out.write_all("id,".as_bytes())?;
-    out.write_all(headers.join(",").as_bytes())?;
+    out.write_all(
+        headers
+            .iter()
+            .map(|h| {
+                if uniques[h].is_empty().not() {
+                    format!("id_{h}")
+                } else {
+                    h.to_owned()
+                }
+            })
+            .collect::<Vec<_>>()
+            .join(",")
+            .as_bytes(),
+    )?;
     out.write_all("\n".as_bytes())?;
     for (n, line) in file.lines().skip(1).enumerate() {
         let line = line?;
@@ -75,22 +131,35 @@ fn main() -> Result<()> {
         out.write_all(&[b'\n'])?;
     }
 
-    println!("Wrote csv with replaced foreing keys to './results/out.csv'");
+    println!(
+        "{:>SPACING$} Wrote csv with replaced foreing keys to './results/out.csv'",
+        "Status"
+    );
 
     for header in headers.iter() {
         if uniques[header.as_str()].is_empty().not() {
-            let path = format!("./results/{header}.csv");
+            let header_f = if header.contains("mes") {
+                "mes"
+            } else {
+                header
+            };
+
+            let path = format!("./results/{header_f}.csv");
+
             let mut file = OpenOptions::new()
                 .truncate(true)
                 .write(true)
                 .create(true)
                 .open(path.as_str())?;
 
-            file.write_all(format!("id_{header},{header}\n").as_bytes())?;
-            for (i, value) in uniques[header.as_str()].iter().enumerate() {
+            file.write_all(format!("id_{header_f},{header_f}\n").as_bytes())?;
+            for (i, value) in uniques[header].iter().enumerate() {
                 file.write_all(format!("{i},{value}\n").as_bytes())?
             }
-            println!("Wrote to {path} header values {header} with fields (id_{header}, {header})");
+            println!(
+                "{:>SPACING$} Wrote to '{path}' header values {header_f} with fields (id_{header_f}, {header_f})",
+                "Status"
+            );
         }
     }
 
