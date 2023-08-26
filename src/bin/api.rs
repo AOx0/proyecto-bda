56619a13b7ac0fecd96c798af872f7400ba99e76
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-08-25 20:47:47 -0600

    Add sqlx pool to state

--- src/bin/api.rs
+++ src/bin/api.rs
@@ -1,5 +1,10 @@
 use axum::{http::StatusCode, response::IntoResponse, routing::get, Json, Router, Server};
 use serde::{Deserialize, Serialize};
+use sqlx::mysql:: MySqlPoolOptions;
+use sqlx::{MySql, Pool};
+use dotenv::dotenv;
+use std::sync::Arc;
+use anyhow::Result;
 
 #[derive(Serialize, Deserialize)]
 struct Test<'a> {
@@ -10,12 +15,35 @@ async fn hello() -> Json<Test<'static>> {
     Test { nombre: "A" }.into()
 }
 
-#[tokio::main]
-async fn main() {
-    let router = Router::new().route("/", get(hello));
+#[derive(Clone)]
+struct State {
+    inner: Arc<Inner>
+}
 
-    Server::bind(&"[::]:80".parse().unwrap())
+struct Inner {
+    db: Pool<MySql>
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenv().ok();
+    
+    let db = dotenv::var("DATABASE_URL").unwrap();
+    
+    let state = State {
+        inner: Arc::new( Inner {
+            db: MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect(&db).await?
+        })
+    };
+    
+     let router = Router::new().route("/", get(hello))
+        .with_state(state);
+
+ Server::bind(&"[::]:80".parse().unwrap())
         .serve(router.into_make_service())
-        .await
-        .unwrap()
+        .await?;
+        
+    Ok(())
 }
