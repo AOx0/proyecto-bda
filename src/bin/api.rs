e9b1c0a44d1dd7be5bc85d0563db8a8d6867a303
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-08-25 22:08:15 -0600

    sqlx test

--- src/bin/api.rs
+++ src/bin/api.rs
@@ -1,32 +1,42 @@
-use axum::{http::StatusCode, response::IntoResponse, routing::get, Json, Router, Server};
 use axum::extract::State;
-use serde::{Deserialize, Serialize};
-use sqlx::mysql:: MySqlPoolOptions;
-use sqlx::{MySql, Pool};
+use axum::routing::get;
+use axum::{Json, Router, Server};
 use dotenv::dotenv;
-use std::sync::Arc;
+use serde::Serialize;
+use sqlx::mysql::MySqlPoolOptions;
+use sqlx::{MySql, Pool};
 use std::borrow::Cow;
+use std::sync::Arc;
 
-
-#[derive(Serialize, Deserialize)]
-struct Test {
+#[derive(Serialize)]
+struct Test<T: Serialize> {
     nombre: Cow<'static, str>,
+    resultado: T,
 }
 
-async fn hello(State(state): State<Shared>) -> Result<Json<Test>, Json<Test>> {
+async fn hello(
+    State(state): State<Shared>,
+) -> Result<Json<Test<impl Serialize>>, Json<Test<impl Serialize>>> {
     let row: Result<(i64,), _> = sqlx::query_as("SELECT DISTINCT YEAR(fecha_hecho) FROM delitos")
-        .fetch_one(&state.db).await;
-        
+        .fetch_one(&state.db)
+        .await;
+
     match row {
-        Ok((row,)) => Ok(Test { nombre: format!("{row:?}").into() }.into()),
-        Err(err) => Err(Test { nombre: format!("{err:?}").into() }.into())
+        Ok((row,)) => Ok(Test {
+            nombre: "SELECT DISTINCT YEAR(fecha_hecho) FROM delitos".into(),
+            resultado: row,
+        }
+        .into()),
+        Err(err) => Err(Test {
+            nombre: "SELECT DISTINCT YEAR(fecha_hecho) FROM delitos".into(),
+            resultado: err.to_string(),
+        }
+        .into()),
     }
-        
-    
 }
 
 #[derive(Clone)]
-struct Shared (Arc<Inner>);
+struct Shared(Arc<Inner>);
 
 impl std::ops::Deref for Shared {
     type Target = Inner;
@@ -37,29 +47,27 @@ impl std::ops::Deref for Shared {
 }
 
 struct Inner {
-    db: Pool<MySql>
+    db: Pool<MySql>,
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenv().ok();
-    
-    let db = dotenv::var("DATABASE_URL").unwrap();
-    
-    let state = Shared (
-        Arc::new( Inner {
-            db: MySqlPoolOptions::new()
-        .max_connections(5)
-        .connect(&db).await?
-        })
-    );
-    
-     let router = Router::new().route("/", get(hello))
-        .with_state(state);
 
- Server::bind(&"[::]:80".parse().unwrap())
+    let db = dotenv::var("DATABASE_URL").unwrap();
+
+    let state = Shared(Arc::new(Inner {
+        db: MySqlPoolOptions::new()
+            .max_connections(5)
+            .connect(&db)
+            .await?,
+    }));
+
+    let router = Router::new().route("/", get(hello)).with_state(state);
+
+    Server::bind(&"[::]:80".parse().unwrap())
         .serve(router.into_make_service())
         .await?;
-        
+
     Ok(())
 }
