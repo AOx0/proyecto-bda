a476177b8b26516a5bbc33b8bd06ead027d5bc10
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-08-28 17:11:55 -0600

    Progress

--- utils/queries.sql
+++ utils/queries.sql
@@ -11,3 +11,20 @@ FROM delitos
     ) AS s
 WHERE latitud 
     BETWEEN (s.mean - 3 * s.std) AND (s.mean + 3 * s.std);
+
+
+SELECT 
+    alcaldia_hecho, 
+    Total 
+FROM (
+    SELECT 
+        id_alcaldia_hecho, 
+        COUNT(*) as Total 
+    FROM delitos 
+       WHERE YEAR(fecha_hecho) = 2023 
+         AND id_categoria IN (1) 
+    GROUP BY id_alcaldia_hecho
+) AS res 
+JOIN alcaldia_hecho 
+ON (alcaldia_hecho.id_alcaldia_hecho = res.id_alcaldia_hecho) 
+ORDER BY Total DESC; 
---
+++ utils/script.js
@@ -0,0 +1,2 @@
+// Set fill color of element 1-1
+document.getElementById('1-1').style.fill = 'yello'
