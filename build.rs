78ecaed1b740f59ba7b0741321861444e7c0f93b
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-09-12 13:26:47 -0600

    Avoid build.rs crash

--- build.rs
+++ build.rs
@@ -1,8 +1,17 @@
 use std::process::Command;
 
+macro_rules! p {
+    ($($tokens: tt)*) => {
+        println!("cargo:warning={}", format!($($tokens)*))
+    }
+}
+
 fn main() {
-    Command::new("tailwindcss")
+    let res = Command::new("tailwindcssd")
         .args(["-i", "./templates/input.css", "-o", "./style.css"])
-        .output()
-        .expect("failed to execute process");
+        .output();
+
+    if let Err(err) = res {
+        p!("Error executing `tailwindcss` {}. Skipping.", err);
+    }
 }
